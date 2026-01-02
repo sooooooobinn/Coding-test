d28bd3fc5d706c2184d9fcc8235d5fa1c3aa9570
sooooooobinn <s25021@gsm.hs.kr>
2026-01-02 23:56:39 +0900

    [Bronze II] Title: 소수 찾기, Time: 176 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+
+            if (num == 1) continue; // 1은 소수가 아님
+
+            boolean isPrime = true;
+
+            for (int j = 2; j < num; j++) {
+                if (num % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) sum++;
+        }
+
+        System.out.println(sum);
+    }
+}
